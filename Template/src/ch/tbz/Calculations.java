4ea080e09788dffdc1c02ba1ecc0e7dcdddf99a4
Jevgenia Shylovich <jevgenia.shylovich@noseryoung.ch>
2024-11-12 11:26:06 +0100

    added the template file

---
+++ Template/src/ch/tbz/Calculations.java
@@ -0,0 +1,26 @@
+package ch.tbz;
+
+public class Calculations {
+
+
+    // Die Funktion main in einer Klasse wird automatisch gestartet
+    public static void main( String[] args ) {
+
+        // Variablen bereitstellen und nullen:
+        double gallons = 0;
+        double litres = 0;
+
+        // Einen Wert speichern:
+        gallons = 10;
+
+        //Das Result berechnen:
+        litres = gallons / 3.7854;
+
+        // Ausgabe des Resultats auf einer Zeile:
+        System.out.println(gallons + " gallons is " + litres + " litres.");
+        System.out.println(gallons + " gallons is " + litres + " litres.");
+        System.out.println(gallons + " gallons is " + litres + " litres.");
+        System.out.println(gallons + " gallons is " + litres + " litres.");
+
+    }
+}
